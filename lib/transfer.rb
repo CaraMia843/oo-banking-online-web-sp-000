3ef03e738bf1b6b7fd759b9ebf50e1f580984327
Cara Zellner <carajeanine@gmail.com>
2020-12-21 17:34:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
      @status = "rejected"
      "Transaction Rejected."
    else
-     @sender.deposit(@amount * + 1)
+     @sender.deposit(@amount * - 1)
      @receiver.deposit(@amount)
      @status = "complete"
    end
