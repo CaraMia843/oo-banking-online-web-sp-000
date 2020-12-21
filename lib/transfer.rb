3ecd0a051ec992de5cb537f2ad1eaeacb902b133
Cara Zellner <carajeanine@gmail.com>
2020-12-21 17:46:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,11 +18,14 @@ class Transfer
  end
  
  def execute_transaction
+   
    if @sender.balance < @amount
       @status = "rejected"
         "Transaction rejected. Please check your account balance."   
+   
     elsif @status == "complete"
        puts "Transaction was already executed"
+    
     else
      @sender.deposit(@amount * -1)
      @receiver.deposit(@amount)
